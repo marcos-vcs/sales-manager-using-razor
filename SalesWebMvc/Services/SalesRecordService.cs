6a54499c7e1f707b8e76f6b673d7d7f8e0279499
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2022-03-17 11:16:01 -0300

    feature/ simple search implementation

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,43 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        //injeção de dependencia 
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? min, DateTime? max)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (min.HasValue)
+            {
+                result = result.Where(x => x.Date >= min.Value);
+            }
+
+            if (max.HasValue)
+            {
+                result = result.Where(x => x.Date <= max.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Departament)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+        }
+
+    }
+}
